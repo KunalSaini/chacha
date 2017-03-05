6228d2a57044f3c9b89c944e7f7fb01f3d25c172
KunalSaini <saini.kunal@gmail.com>
2017-03-05 14:42:19 -0600

    Add border to svg canvas

--- app/editor.js
+++ app/editor.js
@@ -1,8 +1,23 @@
 import * as d3 from 'd3';
 
+console.log(screen.availHeight);
+const viewBox = `0 0 ${screen.availWidth} ${screen.availHeight}`;
+const width = screen.availWidth;
+const height = screen.availHeight;
+
 const editor = d3.select('body').append('svg')
 .attr('id', 'editor')
-.attr('width', 50)
-.attr('height', 50);
+.attr('viewBox', viewBox)
+.attr('width', width)
+.attr('height', height);
+
+editor.append('rect')
+.attr('x', 0)
+.attr('y', 0)
+.attr('height', height)
+.attr('width', width)
+.style('stroke', 'black')
+.style('fill', 'none')
+.style('stroke-width', 5);
 
 export default editor;
