7658dd3ff223476e5af461f2b4d8b10e2041daa0
KunalSaini <saini.kunal@gmail.com>
2017-03-05 12:32:26 -0600

    Include D3 library

--- app/index.js
+++ app/index.js
@@ -1,14 +1,20 @@
-import Account from './Account';
+import * as d3 from 'd3';
+// import Account from './Account';
 
 function component() {
   const element = document.createElement('div');
 
-  element.innerHTML = 'hello world'; // _.join(['Hello t','webpack'], ' ');
+  element.innerHTML = 'hello world';
   return element;
 }
 
-// const myAccount = new Account();
-Account.deposit(50);
-Account.widraw(10);
-
+console.log(d3);
+d3.select('body').append('svg')
+.attr('width', 50)
+.attr('height', 50)
+.append('circle')
+.attr('cx', 25)
+.attr('cy', 25)
+.attr('r', 25)
+.style('fill', 'purple');
 document.body.appendChild(component());
